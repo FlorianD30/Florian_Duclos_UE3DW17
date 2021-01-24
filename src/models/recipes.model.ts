c2dd62e8f63c24ebde4ba6100e724988bc48984e
Jonathan Lesueur <jonathan.lesueur.tic@gmail.com>
2021-01-24 12:58:14 +0100

    main: modification Recipe model

--- src/models/recipes.model.ts
+++ src/models/recipes.model.ts
@@ -1,63 +1,52 @@
-// recipes-model.ts - A mongoose model
+// recipe-model.ts - A mongoose model
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 import { Application } from '../declarations';
 import { Model, Mongoose } from 'mongoose';
-const parse = require('coercion');
 
 export default function (app: Application): Model<any> {
-  const modelName = 'recipes';
-  const mongooseClient: Mongoose = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
-  const requireOwner = parse.boolean(app.get('insecure')) !== true;
-
-  const instructions = new Schema({
-    content: { type: String, required: true }
-  }, { _id: false, autoIndex: false });
-
-  const ingredients = new Schema({
-    ingredientId: { type: Schema.Types.ObjectId, ref: 'ingredients' },
-    quantity: { type: Number, required: true },
-    unit: { type: String, required: true, enum: ['unit', 'g', 'ml'], default: 'unit' }
-  }, { _id: false, autoIndex: false });
-
-  const details = new Schema({
-    instructions: [instructions],
-    ingredients: [ingredients]
-  }, { _id: false, autoIndex: false });
-
-  const recipes = new Schema({
-    name: { type: String, required: true },
-    ownerId: { type: Schema.Types.ObjectId, ref: 'users', required: requireOwner },
-    duration: { type: Number, required: true, default: 10, min: 1 },
-    serving: { type: Number, required: true, default: 1, min: 1 },
-    type: {
-      type: String,
-      required: true,
-      enum: ['link', 'details']
-    },
-    url: {
-      type: String,
-      required: function (this: any) {
-        return this.type == 'link';
-      }
-    },
-    details: {
-      type: details,
-      required: function (this: any) {
-        return this.type == 'details'
-      }
-    },
-    deletedAt: { type: Number, default: -1 }
-  }, {
-    timestamps: true
-  });
-
-  // This is necessary to avoid model compilation errors in watch mode
-  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
-  if (mongooseClient.modelNames().includes(modelName)) {
-    (mongooseClient as any).deleteModel(modelName);
-  }
-  return mongooseClient.model<any>(modelName, recipes);
-}
+    const modelName = 'recipe';
+    const mongooseClient: Mongoose = app.get('mongooseClient');
+    const { Schema } = mongooseClient;
+    
+    const RecipeStepSchema = new Schema({
+        content: {type: String, required: true}
+    });
+    const RecipeIngredientSchema = new Schema({
+        ingredientId: {type: String, required: true},
+        quantity: {type: Number, required: true}
+    });
+    
+    const RecipeSchema = new Schema({
+        name: { type: String, required: true },
+        type: { type: String, required: true, enum: ['link', 'details']},
+        details: { 
+            type: Array,
+            required: [
+                function(this: any) {
+                    return this.type == 'details';
+                }
+            ],
+            instructions: {type: [RecipeStepSchema]},
+            ingredients: {type: [RecipeIngredientSchema]}
+        },
+        url: {
+            type: String,
+            required: [
+                function(this: any) {
+                    return this.type == 'link';
+                }
+            ]}
+        }, {
+            timestamps: true
+        });
+        
+        // This is necessary to avoid model compilation errors in watch mode
+        // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+        if (mongooseClient.modelNames().includes(modelName)) {
+            (mongooseClient as any).deleteModel(modelName);
+        }
+        return mongooseClient.model<any>(modelName, RecipeSchema);
+    }
+    
